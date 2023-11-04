1280056001f1e55fd4a2074b022821dbb320fd77
Rahu1kr <88312053+Rahu1kr@users.noreply.github.com>
2023-11-04 21:22:21 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -5,22 +5,22 @@ contract mytokens {
 
     // public variables here
     string public tokenName = "Rahul";
-    string public token-abb = "Kumar";
-    uint public total-amount = 0;
+    string public token_abb = "Kumar";
+    uint public total_amount = 0;
 
     // mapping variable here
     mapping(address => uint) public bal;
 
     // mint function
     function mint(address _addr, uint _val) public {
-        total-amount += _val;
+        total_amount += _val;
         bal[_addr] += _val;
     }
 
     // burn function
     function burn(address _addr, uint _val) public {
         if(bal[_addr] >= _val) {
-            total-amount -= _val;
+            total_amount -= _val;
             bal[_addr] -= _val;
     }
     }
